6d7337f87de5200924892fa355adf621ccd4d48f
ushamiev <ushamiev.m@gmail.com>
2024-03-03 22:09:45 +0600

    remove http override zaglushka

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,7 @@
-import 'dart:io';
-
 import 'package:auto_route/auto_route.dart';
 import 'package:easy_localization/easy_localization.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:ishker_24/core/constants/app_text_constants.dart';
-import 'package:ishker_24/core/utils/bloc_observer.dart';
 import 'package:ishker_24/core/utils/text_scaler.dart';
 import 'package:ishker_24/routes/mobile_auto_router.dart';
 import 'package:ishker_24/server/service_locator.dart';
@@ -16,21 +11,10 @@ import 'package:ishker_24/widgets/init_widget.dart';
 final scaffoldKey = GlobalKey<ScaffoldMessengerState>();
 final appRouter = sl<AppRouter>();
 
-class MyHttpOverrides extends HttpOverrides {
-  @override
-  HttpClient createHttpClient(SecurityContext? context) {
-    return super.createHttpClient(context)
-      ..badCertificateCallback =
-          (X509Certificate cert, String host, int port) => true;
-  }
-}
-
 void main() async {
   await initServiceLocator();
 
-  Bloc.observer = const AppBlocObserver();
-  // android emu dev mode workaround through HandshakeException CERTIFICATE_VERIFY_FAILED
-  if (kDebugMode) HttpOverrides.global = MyHttpOverrides();
+  // Bloc.observer = const AppBlocObserver();
 
   runApp(const Main());
 }
