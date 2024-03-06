18b36785dd63c05db06f3008a72e4d08ce192b65
Zhoomart Nurlan Uulu <joomart.bedelbaev12@gmail.com>
2024-03-06 14:03:41 +0600

    all fixed

--- lib/features/nalog_decloration/presentation/oep_signature_screens/widgets/nalog_sms_confirm_sheet.dart
+++ lib/features/nalog_decloration/presentation/oep_signature_screens/widgets/nalog_sms_confirm_sheet.dart
@@ -1,11 +1,12 @@
-import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:ishker_24/core/formatters/shapes.dart';
+import 'package:ishker_24/core/functions/push_router_func.dart';
 import 'package:ishker_24/core/images/app_images.dart';
 import 'package:ishker_24/features/register_ip/presentation/cubits/timer_cubit/timer_cubit.dart';
 import 'package:ishker_24/features/tunduk_auth/widgets_general/sms_code_input_widget.dart';
+import 'package:ishker_24/routes/mobile_auto_router.gr.dart';
 import 'package:ishker_24/server/service_locator.dart';
 import 'package:ishker_24/theme/app_colors.dart';
 import 'package:ishker_24/theme/app_text_styles.dart';
@@ -109,6 +110,7 @@ Future<void> nalogSmsConfirmSheet(
                 const SizedBox(height: 16),
                 CustomButton(
                   onPress: () {
+                    Navigator.of(context).pop();
                     if (formKey.currentState!.validate()) {
                       showDialog(
                         context: context,
@@ -132,7 +134,8 @@ Future<void> nalogSmsConfirmSheet(
                               const SizedBox(height: 24),
                               CustomButton(
                                 onPress: () {
-                                  context.router.pop(true);
+                                  AppRouting.popUntilFunction(
+                                      NalogMainRoute.name);
                                 },
                                 text: 'Закрыть',
                               )
