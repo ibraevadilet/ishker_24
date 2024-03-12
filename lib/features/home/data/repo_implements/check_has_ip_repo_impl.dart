59ab3e54a09d6041b9613a6bd200ff573e146c4f
ibraevadilet <ibraevadilet3@gmail.com>
2024-03-12 16:19:33 +0600

    add

--- lib/features/home/data/repo_implements/check_has_ip_repo_impl.dart
+++ lib/features/home/data/repo_implements/check_has_ip_repo_impl.dart
@@ -1,11 +1,9 @@
 import 'package:dio/dio.dart';
 import 'package:ishker_24/core/app_helpers/app_device_info.dart';
+import 'package:ishker_24/core/functions/saved_pin.dart';
 import 'package:ishker_24/features/home/data/models/check_has_ip_model.dart';
 import 'package:ishker_24/features/home/domain/repositories/check_has_ip_repository.dart';
-import 'package:ishker_24/features/splash/domain/use_cases/exists_user_usecase.dart';
-import 'package:ishker_24/features/tunduk_auth/authorization_tunduk/domain/use_cases/auth_usecase.dart';
 import 'package:ishker_24/server/catch_exception.dart';
-import 'package:ishker_24/server/service_locator.dart';
 
 class CheckHasIPRepoImpl implements CheckHasIPRepo {
   final Dio dio;
@@ -32,10 +30,7 @@ class CheckHasIPRepoImpl implements CheckHasIPRepo {
 
   @override
   Future<bool> checkHasBank() async {
-    String isSavedPin = sl<ExistsUserUseCase>().pin;
-    if (isSavedPin.isEmpty) {
-      isSavedPin = sl<AuthUseCase>().inn;
-    }
+    String isSavedPin = AppSavedPin.getPin();
 
     try {
       final response = await dio.post(
